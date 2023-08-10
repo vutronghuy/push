ee9f1b9e8e2b9b739a47ea07a38aa04c760aad73
vutronghuy <vutronghuygrw24092003@gmail.com>
2023-08-10 11:47:01 +0700

    index

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/student');
 var usersRouter = require('./routes/users');
 //khai báo route
 var studentRouter = require('./routes/student');
